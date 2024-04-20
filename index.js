15bf969cf4e23e97d91071dab070715c6bd15b10
meinjoy <meinjoy@outlook.com>
2024-04-20 17:02:27 +0800

    add tips

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
-const _ = require('lodash');
 
+const _ = require('lodash');
 try {
 	// Node js will throw an error
 	this === window;
 
 	const Clipboard = require('clipboard');
-	new Clipboard('.markdown-it-code-copy');
+	var clipboard = new Clipboard('.markdown-it-code-copy');
+
+	clipboard.on('success', function (e) {
+		var element = e.trigger;// 获取触发剪贴板操作的元素
+		var icon = element.querySelector('#copy-id');
+		if (icon) {
+			icon.className = 'mdi mdi-check';
+			icon.textContent = ' copied';
+			setTimeout(function() {
+				icon.className = 'mdi mdi-content-copy';
+				icon.textContent = '';
+			}, 3000);
+		}
+		e.clearSelection();
+	});
+
+	clipboard.on('error', function (e) {
+		console.error('Action:', e.action);
+		console.error('Trigger:', e.text);
+	});
 }
 catch (_err) {
 }
 
 const defaultOptions = {
-	iconStyle: 'font-size: 21px; opacity: 0.4;',
+	iconStyle: 'font-size: 18px; opacity: 0.6; color:white;',
 	iconClass: 'mdi mdi-content-copy',
 	buttonStyle: 'position: absolute; top: 7.5px; right: 6px; cursor: pointer; outline: none;',
 	buttonClass: ''
@@ -32,9 +51,9 @@ function renderCode(origRule, options) {
 		return `
 <div style="position: relative">
 	${origRendered}
-	<button class="markdown-it-code-copy ${options.buttonClass}" data-clipboard-text="${content}" style="${options.buttonStyle}" title="Copy">
-		<span style="${options.iconStyle}" class="${options.iconClass}"></span>
-	</button>
+	<span class="markdown-it-code-copy ${options.buttonClass}" data-clipboard-text="${content}" style="${options.buttonStyle}" title="Copy">
+		<span id="copy-id" style="${options.iconStyle}" class="${options.iconClass}"></span>
+	</span>
 </div>
 `;
 	};
